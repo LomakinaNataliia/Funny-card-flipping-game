62a0b9df8368fd91514f161c6cb43b4fdda8a0ff
LomakinaNataliia <lomakinataliia@gmail.com>
2021-03-02 23:13:00 +0200

    changed css, js and content

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const card = document.querySelectorAll(".card__inner");
+const cardImg = document.querySelectorAll("img");
+console.log(cardImg);
 
 card.forEach((card) => {
   card.addEventListener("click", function () {
     card.classList.toggle("is-flipped");
+    //  showImages();
   });
 });
+
+let animals = new Array();
+for (let i = 1; i < 7; i++) {
+  animals.push(`./img/animal${i}.jpg`);
+}
+//console.log(animals);
+
+for (let i = 0; i < cardImg.length; i++) {
+  cardImg[i].src = animals[animals.length - 3];
+  // for (let j = 0; j < animals.length - 1; j++) {
+  //   cardImg[i].src = animals[j];
+  //   continue;
+  // }
+}
