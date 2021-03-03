b9a409ead85ef86e61679e2b580f64986767bebc
LomakinaNataliia <lomakinataliia@gmail.com>
2021-03-03 15:40:57 +0200

    completed array construction, changed css

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const card = document.querySelectorAll(".card__inner");
+const cards = document.querySelectorAll(".card__inner");
 const cardImg = document.querySelectorAll("img");
 console.log(cardImg);
 
-card.forEach((card) => {
+cards.forEach((card) => {
   card.addEventListener("click", function () {
     card.classList.toggle("is-flipped");
     //  showImages();
@@ -13,12 +13,13 @@ let animals = new Array();
 for (let i = 1; i < 7; i++) {
   animals.push(`./img/animal${i}.jpg`);
 }
+for (let i = 1; i < 7; i++) {
+  animals.push(`./img/animal${i}.jpg`);
+}
 //console.log(animals);
 
 for (let i = 0; i < cardImg.length; i++) {
-  cardImg[i].src = animals[animals.length - 3];
-  // for (let j = 0; j < animals.length - 1; j++) {
-  //   cardImg[i].src = animals[j];
-  //   continue;
-  // }
+  for (let j = 0; j < 1; j++) {
+    cardImg[i].src = animals[i];
+  }
 }
